ec494d46924dbff1b18ddc73010547e9a30f40b8
Shresth Tyagi <129915548+ShresthTyagi1996@users.noreply.github.com>
2025-04-14 17:22:48 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { RecipesSectionComponent } from './recipes-section/recipes-section.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { FAQSectionComponent } from './faq-section/faq-section.component';
+import { CategoryPageComponent } from './category-page/category-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: RecipesSectionComponent
+    },
+    {
+        path: 'category', component: CategoryPageComponent
+    },
+    {
+        path: 'faq', component: FAQSectionComponent
+    },
+    {
+        path: 'add-recipe', component: AddRecipeComponent
+    },
+];
